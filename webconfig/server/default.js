aa1bc2f078a351340c3151e669cb77561cfd6bea
anupamme <mediratta@gmail.com>
2026-08-18 22:48:47 +0000

    fix: javascript.express.security.audit.express-check-csurf-middleware-usage.express-check-csurf-middleware-usage security vulnerability
    Automated security fix generated by OrbisAI Security

--- webconfig/server/default.js
+++ webconfig/server/default.js
@@ -2,10 +2,14 @@
 
     "use strict";
 
-    var express     = require('express'),
-        app         = express(),
-        server      = require('http').createServer(app);
+    var express      = require('express'),
+        app          = express(),
+        server       = require('http').createServer(app),
+        cookieParser = require('cookie-parser'),
+        csrf         = require('csurf');
 
+    app.use(cookieParser());
+    app.use(csrf({ cookie: true }));
     app.use(express.static(__dirname + '/..'));
     server.listen($process.env.PORT || 3507);
 
